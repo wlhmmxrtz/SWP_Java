22d49d64491c382f33dd39ae3959419307c2aef2
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2021-12-22 09:20:44 +0100

    finally finished LetterCounter :P

---
+++ src/at/mow/project/basics1/LetterCounter.java
@@ -0,0 +1,40 @@
+package at.mow.project.basics1;
+
+import org.w3c.dom.Text;
+
+import java.util.Scanner;
+
+public class LetterCounter {
+    public static void main(String args[]) {
+        Scanner insertScan = new Scanner(System.in);
+
+        System.out.println("Geben sie einen Satz ein");
+        String originalText = insertScan.nextLine();
+
+
+        char[] Text1 = originalText.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < Text1.length; i++) {
+            count++;
+
+        }
+        System.out.println("der Text ist " + count + " Zeichen lang");
+        System.out.println("Nach welchem Buchstaben wollen sie suchen? - bitte beachten sie die Groß- und Kleinschreibung!");
+
+
+        char check = insertScan.next().charAt(0);
+
+
+        int timesLetterIsUsed = 0;
+
+        for (int i = 0; i < Text1.length; i++) {
+            if (Text1[i] == check) {
+                timesLetterIsUsed++;
+            }
+        }
+        System.out.println("Der Buchstabe " + check + " wird " + timesLetterIsUsed + " mal benutzt.");
+        return;
+    }
+
+}
