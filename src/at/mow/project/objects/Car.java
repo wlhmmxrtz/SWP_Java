11946d375fb5c99e93e79a716025a0d966d427c0
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-01-21 11:25:41 +0100

    Edited Main.java and made a constructor in Car.java

--- src/at/mow/project/objects/Car.java
+++ src/at/mow/project/objects/Car.java
@@ -1,16 +1,61 @@
 package at.mow.project.objects;
 
+import java.util.Scanner;
+
 public class Car {
 
     public int fuelConsumption;
     public int fuelAmount;
+    public int fuelVolume;
     public String brand;
     public String model;
     public String serialNumber;
     public String color;
 
+
+    public Car(int fuelConsumption, int fuelAmount, int fuelVolume, String brand, String model, String serialNumber, String color){
+        this.fuelConsumption = fuelConsumption;
+        this.fuelAmount = fuelAmount;
+        this.fuelVolume = fuelVolume;
+        this.brand = brand;
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.color = color;
+    }
+
     public void drive(){
         this.fuelAmount = this.fuelAmount - fuelConsumption;
-        System.out.println("currently driving...");
+        System.out.println(serialNumber + " Is currently diving");
     }
+    public void brake(){
+        String brake;
+        brake = "Ich bremse";
+        System.out.println(brake);
+    }
+
+    public void turboBoost(){
+        if(fuelVolume*0.1 < fuelAmount){
+            System.out.println("SuperBoostMode activated");
+        } else{
+            System.out.println("Not enough fuel to go to Superboost.");
+        }
+    }
+
+    public void honk(){
+        System.out.println("Please enter the Amount of times you want to honk");
+        Scanner scanner = new Scanner(System.in);
+
+        int amount = scanner.nextInt();
+
+        for (int amountOfRepetitions = 0;  amountOfRepetitions < amount; amountOfRepetitions++) {
+            System.out.println("TUUUUUUUUUUUT");
+        }
+    }
+
+    public void getRemainingRange(){
+        int remainingRange = this.fuelAmount/fuelConsumption;
+        System.out.println("There are about " + remainingRange*100 + "km of Range remaining");
+    }
+
+
 }
--- src/at/mow/project/objects/Main.java
+++ src/at/mow/project/objects/Main.java
@@ -4,13 +4,14 @@ import at.mow.project.objects.Car;
 public class Main {
     public static void main(String args[]) {
 
-        Car c1 = new Car();
+     /*   Car c1 = new Car();
         c1.brand = "Lotus";
         c1.model = "Esprit";
         c1.serialNumber = "LE1234";
         c1.color = "white";
         c1.fuelConsumption = 10;
         c1.fuelAmount = 72;
+        c1.fuelVolume = 200;
 
         Car c2 = new Car();
         c2.brand = "Renault";
@@ -19,6 +20,7 @@ public class Main {
         c2.color = "bright red";
         c2.fuelConsumption = 8;
         c2.fuelAmount = 45;
+        c2.fuelVolume = 120;
 
         Car c3 = new Car();
         c3.brand = "Lamborghini";
@@ -27,12 +29,21 @@ public class Main {
         c3.color = "white";
         c3.fuelConsumption = 19;
         c3.fuelAmount = 96;
+        c3.fuelVolume = 250;
+*/
+        Car c4 = new Car(17, 102,322, "Ford", "Raptor", "FR3342", "blue");
 
+        System.out.println(c4.brand + " " + c4.model + " " + c4.serialNumber);
+
+/*
         System.out.println(c1.fuelAmount);
         c1.drive();
         System.out.println(c1.fuelAmount);
+        c1.brake();
+        c1.turboBoost();
+        c1.honk();
+        c1.getRemainingRange();
 
-
-
+*/
     }
 }
