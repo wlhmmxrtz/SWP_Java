9489ef79c70bad45aaf862bf81334fd67710f7a6
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-04-08 11:55:46 +0200

    started creating cars

--- src/at/mow/project/examples/cars/Main.java
+++ src/at/mow/project/examples/cars/Main.java
@@ -1,4 +1,13 @@
 package at.mow.project.examples.cars;
 
+import at.mow.project.examples.cars.Car;
+
 public class Main {
+    public static void main(String args[]) {
+
+        Engine e1 = new Engine(250, "GASOLINE");
+
+        Car c1 = new Car("red", 255, 9, 65000, e1);
+
+    }
 }
