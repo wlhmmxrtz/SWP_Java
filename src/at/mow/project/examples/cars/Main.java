c812580ddc36e5fcf0076e04e63a868e56af8c67
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-04-08 12:05:06 +0200

    created a new Car with Engine and Producer

--- src/at/mow/project/examples/cars/Main.java
+++ src/at/mow/project/examples/cars/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
         Engine e1 = new Engine(250, "GASOLINE");
 
-        Car c1 = new Car("red", 255, 9, 65000, e1);
+        Producer p1 = new Producer("Audi", "Q3 Sportback", "Germany", 8);
+
+        Car c1 = new Car("red", 255, 9, 65000, e1, p1);
+
 
     }
 }
