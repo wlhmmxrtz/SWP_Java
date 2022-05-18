deb80b232be0608d2f1f13086af20a4a6586e4fe
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-05-18 09:57:49 +0200

    Added all functions for Calculator and it's now working

--- src/at/mow/project/calculator/Calculator.java
+++ src/at/mow/project/calculator/Calculator.java
@@ -1,8 +1,149 @@
 package at.mow.project.calculator;
 
+import java.util.Scanner;
+
+
+import java.lang.Math;
+
 public class Calculator {
 
-    private Boolean ergebnis;
+    private double Wert1 = 0;
+    private double Wert2 = 0;
+
+    private double Ergebnis = 0;
 
 
+    public Calculator(double Wert1, double Wert2, double Ergebnis){
+        this.Wert1 = Wert1;
+        this.Wert2 = Wert2;
+        this.Ergebnis = Ergebnis;
+    }
+
+    public double getWert1() {
+        return Wert1;
+    }
+
+    public void setWert1(double wert1) {
+        Wert1 = wert1;
+    }
+
+    public double getWert2() {
+        return Wert2;
+    }
+
+    public void setWert2(double wert2) {
+        Wert2 = wert2;
+    }
+
+    public double getErgebnis() {
+        return Ergebnis;
+    }
+
+    public void setErgebnis(double ergebnis) {
+        Ergebnis = ergebnis;
+    }
+
+
+    public void addNumbers() {
+        System.out.println("Geben sie ihre erste Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        System.out.println("Geben sie ihre zweite Zahl ein");
+        Scanner numberScan2 = new Scanner(System.in);
+        setWert2(numberScan2.nextDouble());
+
+        setErgebnis(getWert1() + getWert2());
+
+        System.out.println("Das Ergebnis ist " + getErgebnis());
+    }
+
+    public void subtractNumbers() {
+        System.out.println("Geben sie ihre erste Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        System.out.println("Geben sie ihre zweite Zahl ein");
+        Scanner numberScan2 = new Scanner(System.in);
+        setWert2(numberScan2.nextDouble());
+
+        setErgebnis(getWert1() - getWert2());
+
+        System.out.println("Das Ergebnis ist " + getErgebnis());
+    }
+
+    public void multiplyNumbers() {
+        System.out.println("Geben sie ihre erste Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        System.out.println("Geben sie ihre zweite Zahl ein");
+        Scanner numberScan2 = new Scanner(System.in);
+        setWert2(numberScan2.nextDouble());
+
+        setErgebnis(getWert1() * getWert2());
+
+        System.out.println("Das Ergebnis ist " + getErgebnis());
+    }
+
+    public void divideNumbers() {
+        System.out.println("Geben sie ihre erste Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        System.out.println("Geben sie ihre zweite Zahl ein");
+        Scanner numberScan2 = new Scanner(System.in);
+        setWert2(numberScan2.nextDouble());
+
+        setErgebnis(getWert1() / getWert2());
+
+        System.out.println("Das Ergebnis ist " + getErgebnis());
+    }
+
+    public void percentOfNumber() {
+        System.out.println("Wie viel Prozent sind ");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        System.out.println("von");
+        Scanner numberScan2 = new Scanner(System.in);
+        setWert2(numberScan2.nextDouble());
+
+        setErgebnis((getWert1()*100) / getWert2());
+
+        System.out.println("Das Ergebnis ist " + getErgebnis() + "%");
+    }
+
+    public void sinusOfNumber() {
+        System.out.println("Geben sie eine Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        double sinRadians = Math.toRadians(getWert1());
+
+        System.out.println("Der Sinus von " + getWert1() + " ist  " + Math.sin(sinRadians));
+    }
+
+    public void cosinusOfNumber() {
+        System.out.println("Geben sie eine Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        double cosRadians = Math.toRadians(getWert1());
+
+        System.out.println("Der Cosinus von " + getWert1() + " ist  " + Math.cos(cosRadians));
+    }
+
+    public void rootOfNumber() {
+        System.out.println("Geben sie eine Zahl ein");
+        Scanner numberScan1 = new Scanner(System.in);
+        setWert1(numberScan1.nextDouble());
+
+        double rootErgebnis = Math.sqrt(getWert1());
+
+        System.out.println("Das Ergebnis ist " + rootErgebnis);
+    }
 }
+
+
+
--- src/at/mow/project/calculator/Main.java
+++ src/at/mow/project/calculator/Main.java
@@ -1,4 +1,63 @@
 package at.mow.project.calculator;
 
+import at.mow.project.calculator.Calculator;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String args[]) {
+
+    Calculator c1 = new Calculator(0,0,0);
+
+    System.out.println("Bitte Wählen sie eine Rechen-Art");
+
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraktion");
+        System.out.println("3 - Multiplikation");
+        System.out.println("4 - Division");
+        System.out.println("5 - %-Rechner");
+        System.out.println("6 - Sinus");
+        System.out.println("7 - Cosinus");
+        System.out.println("8 - Wurzel");
+
+    Scanner selectionScan = new Scanner(System.in);
+
+    byte selection = selectionScan.nextByte();
+
+    if(selection == 1) {
+        c1.addNumbers();
+    } else
+        if(selection == 2){
+            c1.subtractNumbers();
+        } else
+            if(selection == 3){
+                c1.multiplyNumbers();
+            } else
+                if(selection == 4){
+                    c1.divideNumbers();
+                } else
+                    if(selection == 5){
+                        c1.percentOfNumber();
+                    } else
+                        if(selection == 6){
+                            c1.sinusOfNumber();
+                        } else
+                            if(selection == 7){
+                                c1.cosinusOfNumber();
+                            }else
+                                if(selection == 8){
+                                    c1.rootOfNumber();5
+                                } else {
+                                    System.out.println("Error - keine gültige Eingabe");
+                                }
+
+
+
+
+
+
+
+
+
+}
 }
